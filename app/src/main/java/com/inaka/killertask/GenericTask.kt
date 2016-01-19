ee6f7433d3ed631658fa52ca93c1f7e73fcf7509
ramabit <f.e.ramirez94@gmail.com>
2016-01-19 16:54:41 -0300

    fixing generic task

---
+++ app/src/main/java/com/inaka/killertask/GenericTask.kt
@@ -0,0 +1,39 @@
+package com.inaka.killertask
+
+import android.os.AsyncTask
+
+class GenericTask<T>(killerTask: KillerTask<T>) : AsyncTask<Void, Void, T>() {
+
+    private val something: Something<T>
+    private val callback: DoThis<T>
+    private var exception: Exception? = null
+
+    init {
+        something = killerTask.something as Something<T>
+        callback = killerTask.callback as DoThis<T>
+    }
+
+    override fun doInBackground(vararg params: Void): T? {
+        try {
+            return something.whichDoes()
+        } catch (e: Exception) {
+            exception = e
+            return null
+        }
+
+    }
+
+    override fun onPostExecute(result: T) {
+
+        if (!isCancelled) {
+            if (exception != null) {
+                callback.ifNotOK(exception as Exception)
+            } else {
+                callback.ifOK(result)
+            }
+        } else {
+            callback.ifNotOK(RuntimeException("task was cancelled"))
+        }
+
+    }
+}
