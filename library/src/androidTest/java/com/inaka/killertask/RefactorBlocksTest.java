21ccb19bd6fecac4bac03c0f9c52b135e91cfdc8
ramabit <f.e.ramirez94@gmail.com>
2016-02-25 16:11:15 -0300

    adapt to kotlin 1.0.0

--- library/src/androidTest/java/com/inaka/killertask/RefactorBlocksTest.java
+++ library/src/androidTest/java/com/inaka/killertask/RefactorBlocksTest.java
@@ -4,9 +4,6 @@ import android.test.AndroidTestCase;
 import android.test.UiThreadTest;
 
 
-/**
- * Created by inaka on 1/21/16.
- */
 public class RefactorBlocksTest extends AndroidTestCase {
 
     @UiThreadTest
